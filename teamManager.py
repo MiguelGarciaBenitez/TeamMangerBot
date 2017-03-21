b0d03b89d5be9edb6c779271becf9aada4dac5ff
miguel <miguesnk@gmail.com>
2017-03-21 21:56:45 +0100

    polling funciont added

--- teamManager.py
+++ teamManager.py
@@ -365,4 +365,16 @@ def command_Read_Players(m):
         print "ERROR: read players"
         command_Write_Players(m, players)
         return players
-bot.polling()
+def telegram_polling():
+    try:
+        bot.polling(none_stop=True, timeout=60) #constantly get messages from Telegram
+    except:
+        traceback_error_string=traceback.format_exc()
+        with open("Error.Log", "a") as myfile:
+            myfile.write("\r\n\r\n" + time.strftime("%c")+"\r\n«ERROR polling»\r\n"+ traceback_error_string + "\r\n«ERROR polling»")
+        bot.stop_polling()
+        time.sleep(10)
+        telegram_polling()
+
+if __name__ == '__main__':
+    telegram_polling()
